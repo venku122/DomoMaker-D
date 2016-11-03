7e7073af0329579872847c907cbb4328d96826c3
venku122 <tarazevits@gmail.com>
2016-11-02 21:43:58 -0400

    DomoMaker-A Ready to run on Heroku

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+// pull in our routes
+const router = require('./router.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(favicon(`${__dirname}/../client/img/favicon.png`));
+app.use(cookieParser());
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
---
+++ src/controllers/Account.js
@@ -0,0 +1,73 @@
+const models = require('../models');
+
+const Account = models.Account;
+
+const loginPage = (req, res) => {
+  res.render('login');
+};
+
+const signupPage = (req, res) => {
+  res.render('signup');
+};
+
+const logout = (req, res) => {
+  res.redirect('/');
+};
+
+const login = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const username = `${req.body.username}`;
+  const password = `${req.body.pass}`;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'RAWR! All Fields are required' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username or password' });
+    }
+
+    return res.json({ redirect: '/maker' });
+  });
+};
+
+const signup = (request, response) => {
+  const req = request;
+  const res = response;
+
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! All Fields are required' });
+  }
+
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! Passwords do not match' });
+  }
+
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+    const accountData = {
+      username: req.body.username,
+      salt,
+      password: hash,
+    };
+
+    const newAccount = new Account.AccountModel(accountData);
+
+    newAccount.save((err) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ error: 'An error occurred' });
+      }
+
+      return res.json({ redirect: '/maker' });
+    });
+  });
+};
+
+module.exports.loginPage = loginPage;
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.signupPage = signupPage;
+module.exports.signup = signup;
